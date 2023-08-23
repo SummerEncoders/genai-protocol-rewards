a1b783db25b3c997df27015853bf1944912676bd
Ivan Molto <ivanmolto@gmail.com>
2023-08-23 09:38:09 +0200

    chore: Add RainbowKit provider and Wagmi config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,79 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import {
+  mainnet,
+  goerli,
+  optimism,
+  optimismGoerli,
+  base,
+  baseGoerli,
+  zora,
+  zoraTestnet,
+} from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    optimism,
+    base,
+    zora,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
+      ? [goerli, optimismGoerli, baseGoerli, zoraTestnet]
+      : []),
+  ],
+  [publicProvider()],
+);
+
+const projectId = "79e68291741ebb0e75f64ca1ecaf5770";
+
+const { wallets } = getDefaultWallets({
+  appName: "App Name",
+  projectId,
+  chains,
+});
+
+const customAppInfo = {
+  appName: "App Name",
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Other",
+    wallets: [
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider appInfo={customAppInfo} chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
