fa01f0d957d718164efee62d380ccc0a9c59eee8
Ivan Molto <ivanmolto@gmail.com>
2023-08-26 02:27:14 +0200

    chore: Update blurb and margin top

--- pages/features.tsx
+++ pages/features.tsx
@@ -29,7 +29,7 @@ const features = [
   {
     name: "Monetize Your Work",
     description:
-      "With Protocol Rewards we provide the best experience to create and earn onchain.",
+      "With Zora Protocol Rewards we provide the best experience to earn onchain.",
     icon: BanknotesIcon,
   },
   {
@@ -48,7 +48,7 @@ const features = [
 
 export default function Features() {
   return (
-    <div className="bg-white py-24 sm:py-32 mb-64">
+    <div className="bg-white py-24 sm:py-24 mb-64 mt-24">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
