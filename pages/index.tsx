2c1a08c36c2bda5a9e68c86eefc87bc5239fe1b3
Ivan Molto <ivanmolto@gmail.com>
2023-08-27 22:47:03 +0200

    feat: Enable Create NFT button

--- pages/index.tsx
+++ pages/index.tsx
@@ -209,7 +209,7 @@ export default function Home() {
                 {error && <div>{error}</div>}
                 <button
                   onClick={() => write?.()}
-                  disabled={true}
+                  disabled={false}
                   className="rounded-xl bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                 >
                   Create NFT
